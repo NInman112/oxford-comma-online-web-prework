9e034e189c1135cd0af9572f65f93a3c880fa83e
Nick Inman <nick@theoatmeal.com>
2018-11-18 17:33:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,9 @@ def oxford_comma(array)
   
   if array.count == 3
     array[-1] << (" and #{array[-1]}")
-    array.delete_at(3)
     array[0..2].join(", ")
+    array.delete_at(2)
+    
   else
     array.join(" and ")
   end

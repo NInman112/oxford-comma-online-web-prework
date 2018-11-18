3bb5cb9b8a629983ada655f47910ec7ad6271b0f
Nick Inman <nick@theoatmeal.com>
2018-11-18 17:42:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
 
-  elsif array.count >= 3
+  if array.count >= 3
     last_element = ", and #{array[-1]}"
     array.delete_at(-1)
     array[0..-1].join(", ") << last_element
